f5c0aaef6d153aee51546d95a74620a1c0e78e46
omiao <88465145+AnimalRacional@users.noreply.github.com>
2025-10-29 18:31:40 +0000

    Fim teste 9

--- src/campus_app/app/ServiceStarComparator.java
+++ src/campus_app/app/ServiceStarComparator.java
@@ -6,14 +6,6 @@ import dataStructures.Comparator;
 public class ServiceStarComparator implements Comparator<Service> {
     @Override
     public int compare(Service o1, Service o2) {
-        // TODO verificar se é assim ou o2 - o1
-        float diff = o2.getRealRating() - o1.getRealRating();
-        // TODO deve haver maneira melhor de fazer isto, cuidado com casas decimais
-        if(diff < 0) {
-            return (int)Math.floor(diff);
-        } else {
-            // TODO verificar que se forem iguais dá 0
-            return (int)Math.ceil(diff);
-        }
+        return o2.getRating() - o1.getRating();
     }
 }
