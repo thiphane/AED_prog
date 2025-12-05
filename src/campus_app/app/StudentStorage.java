51ec2f9c322e9c4bbb788564007ba85bae179a38
Thiphane <02anymartins@gmail.com>
2025-12-05 14:39:49 +0000

    StudentStorage java doc

--- src/campus_app/app/StudentStorage.java
+++ src/campus_app/app/StudentStorage.java
@@ -11,7 +11,6 @@ import campus_app.entity.student.Student;
 import java.io.*;
 
 public class StudentStorage implements Serializable {
-    // TODO decidir quantidades
     private static final int EXPECTED_COUNTRY_COUNT = 10;
     // We only serialize studentsByCountry because it needs to save the insertion order
     // All students by order of insertion
@@ -19,11 +18,20 @@ public class StudentStorage implements Serializable {
     // All students sorted alphabetically
     transient protected SortedMap<String, Student> alphabeticalStudentsByName;
 
+    /**
+     * Constructor.
+     * O(1)
+     */
     public StudentStorage() {
         this.studentsByCountry = new SepChainHashTable<>(EXPECTED_COUNTRY_COUNT);
         this.alphabeticalStudentsByName = new AVLSortedMap<>();
     }
 
+    /**
+     * Inserting in a Map has Complexity O(log(n))
+     *
+     * @param student to be inserted.
+     */
     public void addStudent(Student student) {
         String cnty = student.getCountry().toLowerCase();
         ObjectRemovalList<Student> countryList = this.studentsByCountry.get(cnty);
@@ -35,12 +43,28 @@ public class StudentStorage implements Serializable {
         this.alphabeticalStudentsByName.put(student.getName().toLowerCase(), student); // O(log n)
     }
 
+    /**
+     * Has time complexity O(log(n))
+     * @param student to be searched
+     * @return student, if its found
+     * @throws StudentDoesNotExistException when student does not exist
+     */
     public Student getStudent(String student) throws StudentDoesNotExistException {
         Student s = this.alphabeticalStudentsByName.get(student.toLowerCase());
         if ( s == null ) { throw new StudentDoesNotExistException(); }
         return s;
     }
 
+    /**
+     * Time complexity is O(n), for worst case because we have to remove student
+     * from country list, best case has Complexity O(log(n))-from removing from the sortedMap-
+     * if the student is the first in the country list.
+     *
+     *
+     * @param student to remove.
+     * @return removed student, if student its found.
+     * @throws StudentDoesNotExistException when student does not exist.
+     */
     public Student removeStudent(Student student) throws StudentDoesNotExistException {
         Student removed = alphabeticalStudentsByName.remove(student.getName().toLowerCase()); // O(log n)
         if(removed == null) throw new StudentDoesNotExistException();
@@ -49,10 +73,19 @@ public class StudentStorage implements Serializable {
         return removed;
     }
 
+    /**
+     *
+     * @return Student list ordered by their names.
+     */
     public Iterator<Student> getAllStudents() {
         return alphabeticalStudentsByName.values();
     }
 
+    /**
+     *
+     * @param country Country name to be searched
+     * @return An iterator with all students from the given country.
+     */
     public Iterator<Student> getStudentsByCountry(String country) {
         List<Student> c = studentsByCountry.get(country.toLowerCase());
         if ( c == null ) { return new EmptyIterator<>(); }
